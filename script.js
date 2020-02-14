9cfae873c7e549b11f345863fa0bade3ab4a4931
Sal <salvadorchaveztrejo@gmail.com>
2020-02-13 21:06:25 -0800

    replaced Edamam with MealDB, url substring

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ u("#searchButton").on("click", function (){
   //declares ingredient as user input//
   var ingredient = document.querySelector("#ingredientSearch").value;
 
-  var queryURL = "https://api.edamam.com/search?q=" + ingredient + "&app_id=00be0051&app_key=e1c84d55d257f3237966c18638b794e2";
+  var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + ingredient;
   console.log(ingredient)
   console.log(queryURL)
 
@@ -21,14 +21,13 @@ u("#searchButton").on("click", function (){
   .then(function (response){
     console.log (response)
 
-    for(i = 0; i< response.data.hits.length; i++){
-      //console.log(response.data.hits[i].recipe.label)
+    for(i = 0; i< response.data.meals.length; i++){
+      //mealDB api JSON reference
+      var rLabel = response.data.meals[i].strMeal;
+      var rImage = response.data.meals[i].strMealThumb;
+      var rList = response.data.meals[i].strInstructions;
+      var rVideo = response.data.meals[i].strYoutube;
 
-      var rLabel = response.data.hits[i].recipe.label;
-      var rImage = response.data.hits[i].recipe.image;
-      var rList = response.data.hits[i].recipe.ingredientLines;
-      console.log(rList)
-      
       var outputDiv = u(".searchResult").append("<div>");
       var recipeTitle = u("<h4>").text(rLabel);
       outputDiv.append(recipeTitle)
@@ -36,19 +35,20 @@ u("#searchButton").on("click", function (){
       var recipePreview = u("<img>").attr("src", rImage);
       recipePreview.attr("style", "width:150px;height:150px;")
       outputDiv.append(recipePreview)
-      var ingredientTitle = u("<h4>Ingredient List</h4>");
-      outputDiv.append(ingredientTitle);
-      var ulIngredients = u("<ul>");
-      //var sampleli = u("<li>ingredient 1</li>");
-      //ulIngredients.append(sampleli);
-      outputDiv.append(ulIngredients);
 
-      for(j = 0; j < rList.length; j++) {
-        var a = u("<li>");
-        a.text(rList[j])
-        ulIngredients.append(a)
-        //outputDiv.append(ulIngredients)
-      }
+      var ingredientTitle = u("<h4>Instructions</h4>");
+      outputDiv.append(ingredientTitle);
+      //var ulIngredients = u("<ul>");
+      var instructions = u("<p>").text(rList);
+      outputDiv.append(instructions)
+
+      //outputDiv.append(ulIngredients);
+
+      var URLcut = rVideo.substring(24);
+      var embedURL = "https://www.youtube.com/embed/" + URLcut;
+
+      var videourl = u("<iframe>").attr("src", embedURL);
+      outputDiv.append(videourl)
       
     }
 
