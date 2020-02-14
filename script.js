aa37aef82162b0d65c77c00050075d8206b56565
Sal <salvadorchaveztrejo@gmail.com>
2020-02-13 17:07:47 -0800

    loop through hits array, css master branch merge

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ u("#searchButton").on("click", function (){
   //declares ingredient as user input//
   var ingredient = document.querySelector("#ingredientSearch").value;
 
-  var queryURL = "https://api.edamam.com/search?q=" + ingredient + "&app_id=d8c609a2&app_key=c21b71e7d78091b0a0e5c2efc3410557";
+  var queryURL = "https://api.edamam.com/search?q=" + ingredient + "&app_id=00be0051&app_key=e1c84d55d257f3237966c18638b794e2";
   console.log(ingredient)
   console.log(queryURL)
 
@@ -21,38 +21,38 @@ u("#searchButton").on("click", function (){
   .then(function (response){
     console.log (response)
 
-    //retrieve data from api//
-    var rLabel = response.data.hits[0].recipe.label;
-    var rImage = response.data.hits[0].recipe.image;
-    var rList = response.data.hits[0].recipe.ingredientLines;
+    for(i = 0; i< response.data.hits.length; i++){
+      console.log(response.data.hits[i].recipe.label)
 
-    //creating div to display api data//
-    var outputDiv = u(".searchResult").append("<div>");
+      var rLabel = response.data.hits[i].recipe.label;
+      var rImage = response.data.hits[i].recipe.image;
+      //var rList = response.data.hits[i].recipe.ingredientLines;
+      
+      var outputDiv = u(".searchResult").append("<div>");
+      var recipeTitle = u("<h4>").text(rLabel);
+      outputDiv.append(recipeTitle)
 
-    var recipeTitle = u("<h4>").text(rLabel);
-    outputDiv.append(recipeTitle)
-
-    var recipePreview = u("<img>").attr("src", rImage);
-    recipePreview.attr("style", "width:150px;height:150px;")
-    outputDiv.append(recipePreview)
-
+      var recipePreview = u("<img>").attr("src", rImage);
+      recipePreview.attr("style", "width:150px;height:150px;")
+      outputDiv.append(recipePreview)
+    }
 
 
     //declaring array as ingredientLines//
-    ingredientList =rList;
-    console.log(ingredientList)
+    //ingredientList =rList;
+    //console.log(ingredientList)
 
     // appending ingredient list to output div//
-    outputDiv.append("<h4>Ingredient List</h3>")
-    var ulIngredients = outputDiv.append("<ul>");
+    //outputDiv.append("<h4>Ingredient List</h3>")
+    //var ulIngredients = outputDiv.append("<ul>");
 
     //for loop to create li for each ingredient//
-    for(i = 0; i < ingredientList.length; i++) {
-      var a = u("<li>");
-      a.text(ingredientList[i])
-      console.log(ingredientList[i])
-      ulIngredients.append(a)
-    }
+    //for(i = 0; i < ingredientList.length; i++) {
+      //var a = u("<li>");
+      //a.text(ingredientList[i])
+      //console.log(ingredientList[i])
+      //ulIngredients.append(a)
+    //}
   })
 
 })
