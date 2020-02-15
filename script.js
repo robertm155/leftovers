2838c3f801b47a85250bae9b04d956c1b43c3751
Sal <salvadorchaveztrejo@gmail.com>
2020-02-15 12:13:39 -0800

    CocktailDB ajax call, classes for dynamic elements,  HTML checkbox, conditional for checkbox

--- script.js
+++ script.js
@@ -1,58 +1,12 @@
-var ingredient;
-
-var ingredientList = [];
-
 
 u("#searchButton").on("click", function (){
   clearResults()
 
-  //declares ingredient as user input//
-  var ingredient = document.querySelector("#ingredientSearch").value;
-  
-  var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + ingredient;
-  console.log(ingredient)
-  console.log(queryURL)
-
-
-  //empty array//
-  ingredientList = [];
-
-  axios.get(queryURL)
-  .then(function (response){
-    console.log (response)
-
-    for(i = 0; i< response.data.meals.length; i++){
-      //mealDB api JSON reference
-      var rLabel = response.data.meals[i].strMeal;
-      var rImage = response.data.meals[i].strMealThumb;
-      var rList = response.data.meals[i].strInstructions;
-      var rVideo = response.data.meals[i].strYoutube;
-
-      var outputDiv = u(".searchResult").append("<div>");
-      var recipeTitle = u("<h4>").text(rLabel);
-      outputDiv.append(recipeTitle)
-
-      var recipePreview = u("<img>").attr("src", rImage);
-      recipePreview.attr("style", "width:150px;height:150px;")
-      outputDiv.append(recipePreview)
-
-      var ingredientTitle = u("<h4>Instructions</h4>");
-      outputDiv.append(ingredientTitle);
-      //var ulIngredients = u("<ul>");
-      var instructions = u("<p>").text(rList);
-      outputDiv.append(instructions)
-
-      //outputDiv.append(ulIngredients);
-
-      var URLcut = rVideo.substring(24);
-      var embedURL = "https://www.youtube.com/embed/" + URLcut;
-
-      var videourl = u("<iframe>").attr("src", embedURL);
-      outputDiv.append(videourl)
-      
-    }
-
-  })
+  if (u("#thirst-filter").is(':checked')){
+    cocktailDB()
+  }else{
+    mealDB()
+  }
 
 })
 
@@ -64,4 +18,69 @@ u("#clearBtn").on("click", function() {
 
 function clearResults() {
   u(".searchResult").empty();
+}
+
+function mealDB() {
+  var ingredient = document.querySelector("#ingredientSearch").value;
+  var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + ingredient;
+
+  axios.get(queryURL)
+  .then(function (response){
+
+    for(i = 0; i< response.data.meals.length; i++){
+      // r= response
+      var rTitle = response.data.meals[i].strMeal;
+      var rImage = response.data.meals[i].strMealThumb;
+      var rDescription = response.data.meals[i].strInstructions;
+      var rVideo = response.data.meals[i].strYoutube;
+
+      var outputDiv = u(".searchResult").append("<div>");
+      var recipeTitle = u("<h4>").text(rTitle).addClass("recipe-title");
+      outputDiv.append(recipeTitle)
+
+      var recipePreview = u("<img>").attr("src", rImage).addClass("img-thumbnail");
+      recipePreview.attr("style", "width:150px;height:150px;")
+      outputDiv.append(recipePreview)
+
+      var instructionsTitle = u("<h5>Instructions</h5>").addClass("instructions-title");
+      outputDiv.append(instructionsTitle);
+      var instructions = u("<p>").text(rDescription).addClass("instructions");
+      outputDiv.append(instructions)
+
+      var URLcut = rVideo.substring(24);
+      var embedURL = "https://www.youtube.com/embed/" + URLcut;
+      var videourl = u("<iframe>").attr("src", embedURL).addClass("recipe-video");
+      outputDiv.append(videourl) 
+    }
+
+  })
+}
+
+function cocktailDB() {
+  var ingredient = document.querySelector("#ingredientSearch").value;
+  queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + ingredient;
+
+  axios.get(queryURL)
+  .then(function(response){
+
+    for (j = 0; j < response.data.drinks.length; j++){
+      var rTitle = response.data.drinks[j].strDrink;
+      var rImage = response.data.drinks[j].strDrinkThumb;
+      var rDescription = response.data.drinks[j].strInstructions;
+
+      var outputDiv = u(".searchResult").append("<div>");
+      var recipeTitle = u("<h4>").text(rTitle).addClass("recipe-title");
+      outputDiv.append(recipeTitle)
+
+      var recipePreview = u("<img>").attr("src", rImage);
+      recipePreview.attr("style", "width:150px;height:150px;").addClass("img-thumbnail");
+      outputDiv.append(recipePreview)
+
+      var ingredientTitle = u("<h5>Instructions</h5>").addClass("instructions-title");
+      outputDiv.append(ingredientTitle);
+      var instructions = u("<p>").text(rDescription).addClass("instructions");
+      outputDiv.append(instructions)
+    }
+  })
+
 }
